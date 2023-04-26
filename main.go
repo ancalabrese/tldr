531722387620c2333c5ef413648c1292f25c9cd2
Antonio Calabrese <ancalabrese@google.com>
2023-04-26 21:53:25 +0200

    Listen for user input and show answers

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"bufio"
 	"context"
 	"flag"
 	"fmt"
@@ -21,7 +22,6 @@ var (
 )
 
 func main() {
-
 	inputTokens = flag.String("m", "", textHelpMessage)
 	maxLen = flag.String("l", "s", lenHelpMessage)
 	kb = flag.String("kb", "", kbHelpMessage)
@@ -38,32 +38,72 @@ func main() {
 
 	key := os.Getenv("OPENAI_KEY")
 	ctx := context.Background()
+	inputChan := make(chan string, 1)
+	outputChan := make(chan openai.ChatCompletionResponse, 1)
 	client := openai.NewClient(key)
 	messages := make([]openai.ChatCompletionMessage, 0)
 	messages = append(messages, openai.ChatCompletionMessage{
 		Role:    "system",
-		Content: "Give me the tl;dr",
+		Content: "Create a summary for the provided text. Then answer any user questions about it.",
 	},
 		openai.ChatCompletionMessage{
 			Role:    "user",
 			Content: *inputTokens,
 		})
+	go sendNewChatMessage(ctx, client, messages, outputChan)
 
+	go func() {
+		for {
+			select {
+			case <-ctx.Done():
+				return
+			case msg := <-inputChan:
+				{
+					messages := append(messages, openai.ChatCompletionMessage{
+						Role:    "user",
+						Content: msg,
+					})
+					go sendNewChatMessage(ctx, client, messages, outputChan)
+				}
+			case resp := <-outputChan:
+				{
+					go printResponse(resp)
+					messages = append(messages, openai.ChatCompletionMessage{
+						Role:    "assistant",
+						Content: resp.Choices[0].Message.Content,
+					})
+				}
+			}
+		}
+	}()
+
+	for {
+		scanner := bufio.NewScanner(os.Stdin)
+		scanner.Scan() // use `for scanner.Scan()` to keep reading
+		input := scanner.Text()
+		inputChan <- input
+	}
+}
+
+func sendNewChatMessage(ctx context.Context, llm *openai.Client, chatMessages []openai.ChatCompletionMessage, responseChan chan (openai.ChatCompletionResponse)) {
 	req := openai.ChatCompletionRequest{
-		Model:            openai.GPT3Dot5Turbo,
+		Model:            openai.GPT3Dot5Turbo0301,
 		Temperature:      0,
 		MaxTokens:        1000,
 		TopP:             1,
 		FrequencyPenalty: 0,
 		PresencePenalty:  1,
-		Messages:         messages,
-		Stream:           false,
+		Messages:         chatMessages,
 	}
 
-	resp, err := client.CreateChatCompletion(ctx, req)
+	resp, err := llm.CreateChatCompletion(ctx, req)
 	if err != nil {
 		fmt.Printf("Completion error: %v\n", err)
 		return
 	}
+	responseChan <- resp
+}
+
+func printResponse(resp openai.ChatCompletionResponse) {
 	fmt.Println(resp.Choices[0].Message.Content)
 }
